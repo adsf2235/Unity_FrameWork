fc2c8ae29aa17a2a69c74fd271ae4c59d0bb08e9
adsf2235 <adsf2235@naver.com>
2021-09-30 15:52:39 +0900

    Create Move

---
+++ Unity_FrameWork/Assets/Script/Managers/Managers.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_Instance;
+    static Managers Instance { get { Init(); return Instance; } }
+
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+
+    }
+
+    static void Init()
+    {
+        if (s_Instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject() { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+            DontDestroyOnLoad(go);
+            s_Instance = go.GetComponent<Managers>();
+        }
+
+    }
+}
